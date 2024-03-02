6bab86432d78fdace7cafa7d6a79c971d74a85ee
Andrew Yacoub <a.yacoub@qoyod.com>
2024-03-02 17:41:07 +0200

    Adding actors, reviews and locations to the overview page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,10 @@
 class MoviesController < ApplicationController
   def index
+    @movies = Movie.includes(:actors, :reviews, :filming_locations)
     if params[:search].present?
-      @movies = Movie.joins(:actors).where('actors.name LIKE ?', "%#{params[:search]}%").distinct
+      @movies = @movies.joins(:actors).where('actors.name LIKE ?', "%#{params[:search]}%").distinct
     else
-      @movies = Movie.left_joins(:reviews)
+      @movies = @movies.left_joins(:reviews)
                      .select('movies.*, AVG(reviews.stars) as average_stars')
                      .group('movies.id')
                      .order('average_stars DESC')
